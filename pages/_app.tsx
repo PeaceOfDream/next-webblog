b26a9f19ff7174f312e422b93963a1f171d02626
Dima Bart <3088742@mail.ru>
2022-08-25 00:09:29 +0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+	<>
+		<Head>
+			<title>My top</title>
+			<link rel="preconnect" href="https://fonts.googleapis.com"/>
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet"/>
+		</Head>
+
+
+		<Component {...pageProps} />
+	</>
+  )
 }
 
 export default MyApp
